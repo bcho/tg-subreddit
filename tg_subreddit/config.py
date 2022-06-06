77ae9dd13e18f8690e6c0766f63cab49d65b8579
hbc <me@hbc.rocks>
2022-06-06 22:42:28 +0800

    feat: add sqlite-rest support (#6)

--- tg_subreddit/config.py
+++ tg_subreddit/config.py
@@ -3,12 +3,48 @@ import os
 
 database_table_name_reddit_post = "reddit_post"
 
+database_driver_sqlite = 'sqlite'
+database_driver_sqlite_rest = 'sqlite-rest'
+_supported_database_drivers = (database_driver_sqlite, database_driver_sqlite_rest)
+
+
+def database_driver() -> str:
+    """Database driver to use."""
+    value = os.getenv("DATABASE_DRIVER") or database_driver_sqlite
+    if value not in _supported_database_drivers:
+        raise ValueError("DATABASE_DRIVER environment variable not set to 'sqlite' or 'sqlite-rest'")
+    return value
+
 
 def database_path() -> str:
     """Database file path to use."""
     value = os.getenv("DATABASE_PATH")
     if not value:
         raise ValueError("DATABASE_PATH environment variable not set")
+    return value
+
+    
+def database_sqlite_rest_url() -> str:
+    """Sqlite-rest base url to use."""
+    value = os.getenv("DATABASE_SQLITE_REST_BASE_URL")
+    if not value:
+        raise ValueError("DATABASE_SQLITE_REST_BASE_URL environment variable not set")
+    return value
+
+
+def database_sqlite_rest_table_name() -> str:
+    """Sqlite-rest table name to use."""
+    value = os.getenv("DATABASE_SQLITE_REST_TABLE_NAME")
+    if not value:
+        raise ValueError("DATABASE_SQLITE_REST_TABLE_NAME environment variable not set")
+    return value
+
+
+def database_sqlite_rest_token() -> str:
+    """Sqlite-rest token to use."""
+    value = os.getenv("DATABASE_SQLITE_REST_TOKEN")
+    if not value:
+        raise ValueError("DATABASE_SQLITE_REST_TOKEN environment variable not set")
     return value
 
 
